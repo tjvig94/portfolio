244eff8c9be5f3cbd5123c95bf21fdc1e614f360
Tim Vigneau <tjvig94@gmail.com>
2021-04-07 17:02:09 -0400

    Created server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./controller');
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
