fc3b548de6688677980452ee8504b0f7e9d99610
Tim Vigneau <tjvig94@gmail.com>
2021-04-08 17:34:07 -0400

    Handlebars generates carousel

--- controller/index.js
+++ controller/index.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
+const { aboutMe, portfolio } = require('../about-me-portfolio');
 
 router.get('/', (req, res) => {
     res.render('homepage');
 });
 
 router.get('/about-me', (req, res) => {
-    res.render('about-me');
+    res.render('about-me', { aboutMe });
 })
 
 router.get('/portfolio', (req, res) => {
-    res.render('portfolio');
+    res.render('portfolio', { portfolio });
 })
 
 router.get('/')
